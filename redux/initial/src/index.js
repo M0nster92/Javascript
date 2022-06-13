9b983620a3a23869f7b6ae6b3f42ba4fa02ae8e9
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2022-06-12 20:14:29 -0400

    basic redux package implementation

--- redux/initial/src/index.js
+++ redux/initial/src/index.js
@@ -1,7 +1,14 @@
-import store from "./store";
+import store from "./custom-redux";
 import { bugAdded, bugResolved } from "./action-creators";
+
+store.subscribe(() => console.log("state changed"));
 
 store.dispatch(bugAdded("newbug1"));
 store.dispatch(bugResolved(1));
 
 console.log(store.getState());
+
+//store.dispatch(bugAdded("newbug1"));
+//store.dispatch(bugResolved(1));
+
+//console.log(store.getState());
