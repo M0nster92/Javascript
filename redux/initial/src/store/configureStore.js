9445e36a110e4f54cae8dd8105dfff70284348da
M0nster92 <fahimalsafi@gmail.com>
2022-06-16 20:49:48 -0400

    server redux calls

--- redux/initial/src/store/configureStore.js
+++ redux/initial/src/store/configureStore.js
@@ -1,6 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducers";
+import logger from "./middleware/logger";
+import api from "./middleware/api";
 
 export default function () {
-  return configureStore({ reducer });
+  return configureStore({
+    reducer,
+    middleware: [...getDefaultMiddleware(), logger, api],
+  });
 }
--- redux/initial/src/store/entities.js
+++ redux/initial/src/store/entities.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import projectReducers from "./project";
 import bugReducers from "./bugs";
+import userReducers from "./user";
 
 export default combineReducers({
   projects: projectReducers,
   bugs: bugReducers,
+  users: userReducers,
 });
